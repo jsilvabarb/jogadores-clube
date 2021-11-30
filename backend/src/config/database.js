163ea7ab7b5c4acd3dad64a5c9ecb23ac803f3a1
Julia Barbosa <59066234+jsilvabarb@users.noreply.github.com>
2021-11-29 23:00:05 -0300

    Configurei a conexão com o banco

---
+++ backend/src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'root123',
+    database: 'jogadores_clube',  
+    define: {
+        underscored: true
+    }
+};
